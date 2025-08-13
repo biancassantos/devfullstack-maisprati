8c6708af42abc499edf938eb1c8589c699859b48
Bianca Santos <biancassantos89@gmail.com>
2025-08-13 15:56:16 -0300

    Revert "atividade 05"
    This reverts commit 672edb0b8cc96acfa5dff2bed72513d9d34cd61a.

--- modulo_3/movies-tmdb/src/components/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-function ErrorMessage({ message }) {
-  return (
-    <div id="error-container">
-      <p>{message}</p>
-    </div>
-  )
-}
-
-export default ErrorMessage
--- modulo_3/movies-tmdb/src/components/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-function Spinner() {
-  return (
-    <div id="spinner">
-    </div>
-  )
-}
-
-export default Spinner
